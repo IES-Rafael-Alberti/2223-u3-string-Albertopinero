a05bd5821e15b1c69be6e410dd05045e6a323cc5
Alberopinero <apinero@iesrafaelalberti.es>
2023-10-31 14:24:41 +0100

    Commit 4 ejercicios

---
+++ src/ejercicio4.py
@@ -0,0 +1,11 @@
+'''Hay un método de cadenas llamado count que es similar a find. Lee la documentación de este método en: * Métodos en ingles * Métodos en castellano
+y escribe el código necesario para invocar a este método y contar el número de veces que una letra aparece en “banana”.'''
+def ContarLetraEnPalabra():
+    letra = "a"
+    contarletra.count(letra)
+
+if __name__ == "__main__":
+    cadena = "banana"
+    letra = "a"
+    contarletra = cadena.count(letra)
+    print(f"La letra '{letra}' aparece {contarletra} veces en '{cadena}'.")
